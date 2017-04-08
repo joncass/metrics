a10958008db80740475c28c7f9cfd89873629484
Jon Cass <joncass@gmail.com>
2017-04-08 13:16:23 -0400

    material-ui first navigation pass

---
+++ src/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {render} from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import Navigation from './navigation.jsx'; // Our custom react component
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+render(<Navigation />, document.getElementById('app'));
---
+++ webpack-dev-server.config.js
@@ -0,0 +1,45 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_PATH = path.resolve(__dirname, 'src/www');
+const NODE_PATH = path.resolve(__dirname, 'node_modules');
+
+const config = {
+    // Entry points to the project
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack/hot/only-dev-server',
+        path.join(__dirname, '/src/app/app.jsx'),
+    ],
+    output: {
+        path: BUILD_PATH,
+        filename: 'app.js',
+    },
+    devServer: {
+        contentBase: 'src/www',
+        devtool: 'eval',
+        hot: true,
+        inline: true,
+        port: 8000,
+        host: 'localhost',
+    },
+    devtool: 'eval',
+    plugins: [
+        // Enables Hot Modules Replacement
+        new webpack.HotModuleReplacementPlugin(),
+        // Allows error warnings but does not stop compiling.
+        new webpack.NoErrorsPlugin(),
+    ],
+    module: {
+        loaders: [
+            {
+                // hot reload and babel parsing for all js(x) files
+                test: /\.jsx?$/,
+                loaders: ['react-hot', 'babel-loader'],
+                exclude: [NODE_PATH],
+            },
+        ],
+    },
+};
+
+module.exports = config;
